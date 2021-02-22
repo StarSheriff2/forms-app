6f63b662ea7bc2ce4d481b8e8a3549cd955edcde
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-02-22 14:17:04 -0600

    add vaidations to USer model

--- re-former/app/models/user.rb
+++ re-former/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 10 }, uniqueness: true
+  validates :email, presence: true, length: { minimum: 4 }, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+  validates :password, presente: true, length: { minimum: 4 }
 end
